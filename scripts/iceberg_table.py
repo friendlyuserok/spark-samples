d8d4c9feec9ce3824fd1849dc00558dc36369e09
Jason Carneiro <jasondavin.dev@gmail.com>
2022-10-01 19:32:51 -0300

    feat: add iceberg table that partition by transformed column (#5)

--- scripts/iceberg_table.py
+++ scripts/iceberg_table.py
@@ -10,49 +10,13 @@ PYTHONPATH=$PWD spark-submit --conf spark.sql.extensions=org.apache.iceberg.spar
 """
 
 from random import randint
-from typing import List
 from uuid import uuid4
 
+from utils.iceberg import add_partition_fields
 from utils.spark_client import SparkClient
 
 DATASET_SIZE = 10000
 TABLE_FULLNAME = 'iceberg.sample.partitioned'
-
-
-def get_partitions_ddl(partitions: List[str]) -> List[str]:
-    """
-    Returns partition definition statement.
-    """
-    if len(partitions) < 1:
-        return []
-
-    formatted_partitions = []
-
-    for partition in partitions:
-        split = partition.split(':')
-        partition_name = split[0]
-        partition_transform = None
-
-        if len(split) > 1:
-            partition_transform = split[1]
-            partition_ddl = f"{partition_transform}({partition_name})"
-            formatted_partitions.append(partition_ddl)
-            continue
-
-        formatted_partitions.append(partition_name)
-
-    return formatted_partitions
-
-
-def add_partition_fields(spark_session, partitions: List[str]):
-    partitions_ddl = get_partitions_ddl(
-        partitions
-    )
-
-    for partition in partitions_ddl:
-        spark_session.sql(
-            f'ALTER TABLE {TABLE_FULLNAME} ADD PARTITION FIELD {partition}'
-        )
 
 
 def main():
@@ -85,7 +49,7 @@ def main():
         .tableProperty('format-version', '2') \
         .createOrReplace()
 
-    add_partition_fields(spark, ['country'])
+    add_partition_fields(spark, TABLE_FULLNAME, ['country'])
 
     dataframe \
         .repartition(4, 'country') \
